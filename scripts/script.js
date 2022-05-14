c0cbbe2dd32746f782e258401b915469478832e0
pavel <pal5555@mail.ru>
2022-05-14 23:32:04 +0300

    event mouse criations card

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+const criationsItem =  document.querySelectorAll('.criations__item')
+ const criationsImg = document.querySelectorAll('.criations__img')
+const  criationsTitle = document.querySelectorAll(".criations__title");
+
+
+
+criationsImg.forEach(function(elem) {
+    elem.addEventListener("mouseover", function() {
+        console.log(elem)
+        elem.style.opacity = "0.3";
+     
+    });
+});
+criationsImg.forEach(function(elem) {
+    elem.addEventListener("mouseout", function() {
+        console.log(elem)
+        elem.style.opacity = "1";
+     
+    });
+});
+
+
+
