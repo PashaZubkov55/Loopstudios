e044c41c0717ce14a09a1f653a847229cc906a0b
pavel <pal5555@mail.ru>
2022-05-22 00:13:12 +0300

    burger menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,9 @@
 const criationsImg = document.querySelectorAll('.criations__img')
 const  criationsTitle = document.querySelectorAll(".criations__title");
-
-
-
+const burger = document.querySelector(".header__burger")
+const headerMobile = document.querySelector(".header__top-mobile")
+const headerTop = document.querySelector('.header__top')
+const close = document.querySelector('.menu__close')
 
 criationsImg.forEach(function(elem) {
     elem.addEventListener("mouseover", function() {
@@ -18,3 +19,14 @@ criationsImg.forEach(function(elem) {
      
     });
 });
+
+burger.addEventListener('click',()=>{
+    headerMobile.style.display = 'flex'
+    headerTop.style.display = 'none'
+
+})
+close.addEventListener('click', ()=>{
+    headerMobile.style.display = 'none'
+    headerTop.style.display = 'flex'
+
+})
